a5340cbb1a4d0184dde3863b14ca33fc9022f1f5
yarden carmi <yardenc183@gmail.com>
2026-03-13 15:42:50 +0200

    Refactor link display logic and enhance course name handling in popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,68 +1,93 @@
 var allLinks = [];
 var visibleLinks = [];
-var j = 0;
+var courseName = "";
+
+function sanitizeFilename(name) {
+  // Replace invalid characters for filenames/folders
+  return name.replace(/[<>:"|?*\x00-\x1f]/g, '_').replace(/\//g, '-');
+}
+
 //Display all visible links.
 function showLinks() {
   var linksTable = document.getElementById('links');
-  j = (visibleLinks.length - (visibleLinks.length/2));
   while (linksTable.children.length > 1) {
     linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
   }
-  for (var i = 0; i < visibleLinks.length/2; ++i) {
-    var row = document.createElement('tr');
-    var col0 = document.createElement('td');
-    var col1 = document.createElement('td');
-    var checkbox = document.createElement('input');
-    checkbox.checked = false;
-    checkbox.type = 'checkbox';
-    checkbox.id = 'check' + i;
-    col0.appendChild(checkbox);
-    col1.innerText = visibleLinks[j];
-    ++j;
-    col1.style.whiteSpace = 'nowrap';
-    col1.onclick = function() {
-      checkbox.checked = !checkbox.checked;
+  for (var s = 0; s < visibleLinks.length; s++) {
+    var section = visibleLinks[s];
+    // Section header
+    var headerRow = document.createElement('tr');
+    var headerCell = document.createElement('td');
+    headerCell.colSpan = 2;
+    headerCell.innerHTML = '<strong>' + section.title + '</strong>';
+    headerCell.style.paddingTop = '10px';
+    headerRow.appendChild(headerCell);
+    linksTable.appendChild(headerRow);
+    // Files
+    for (var f = 0; f < section.files.length; f++) {
+      var row = document.createElement('tr');
+      var col0 = document.createElement('td');
+      var col1 = document.createElement('td');
+      var checkbox = document.createElement('input');
+      checkbox.checked = false;
+      checkbox.type = 'checkbox';
+      checkbox.id = 'check' + s + '_' + f;
+      col0.appendChild(checkbox);
+      col1.innerText = section.files[f].name;
+      col1.style.whiteSpace = 'nowrap';
+      col1.onclick = function() {
+        var cb = this.previousElementSibling.querySelector('input');
+        cb.checked = !cb.checked;
+      };
+      row.appendChild(col0);
+      row.appendChild(col1);
+      linksTable.appendChild(row);
     }
-    row.appendChild(col0);
-    row.appendChild(col1);
-    linksTable.appendChild(row);
   }
 }
 
 // Toggle the checked state of all visible links.
 function toggleAll() {
  var checked = document.getElementById('toggle_all').checked;
-  for (var i = 0; i < visibleLinks.length; ++i) {
-    if (document.getElementById('check' + i))
-    {
-    	document.getElementById('check' + i).checked = checked;
+  for (var s = 0; s < visibleLinks.length; s++) {
+    for (var f = 0; f < visibleLinks[s].files.length; f++) {
+      var cb = document.getElementById('check' + s + '_' + f);
+      if (cb) cb.checked = checked;
     }
   }
 }
 
 // Download all visible checked links.
 function downloadCheckedLinks() {
-  for (var i = 0; i < visibleLinks.length; ++i) {
-    if ((document.getElementById('check' + i)) && (document.getElementById('check' + i).checked)) {
-      chrome.downloads.download({url: visibleLinks[i]},
-                                             function(id) {
-      });
+  for (var s = 0; s < visibleLinks.length; s++) {
+    for (var f = 0; f < visibleLinks[s].files.length; f++) {
+      var cb = document.getElementById('check' + s + '_' + f);
+      if (cb && cb.checked) {
+        chrome.downloads.download({
+          url: visibleLinks[s].files[f].url,
+          filename: sanitizeFilename(courseName) + '/' + sanitizeFilename(visibleLinks[s].title) + '/' + sanitizeFilename(visibleLinks[s].files[f].name)
+        }, function(id) {
+        });
+      }
     }
   }
-  window.close();
+  //window.close();
 }
 
 // Replace the deprecated API //send_links.js is injected into all frames of the active tab
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.type === "links") {
-    var links = message.data;
-    for (var index in links) {
-      allLinks.push(links[index]);
-    }
+    var sections = message.data;
+    courseName = message.courseName || "";
+    allLinks = sections.map(section => ({
+      title: section.sectionTitle,
+      files: section.links.map((link, i) => ({url: link, name: section.file_names[i]}))
+    }));
   }
 
   visibleLinks = allLinks;
   showLinks();
+  document.getElementById('courseName').innerText = courseName;
   // hide or show buttons
   if(!visibleLinks.length)
   {
--- scripts/send_links.js
+++ scripts/send_links.js
@@ -1,39 +1,45 @@
-var file_names = []; 
-var sectionTitlesTable = [];
-var section_titles;
-var links = [].slice.apply(document.getElementsByTagName('a'));
-links = links.map(function(element) {
-if (window.location.href.indexOf("resource") > -1) { //only show the links from "Resources" page	
-  var href = element.href;
-  var hashIndex = href.indexOf('#');
-  if (hashIndex >= 0) 
-  {
-    href = href.toLowerCase().substr(0, hashIndex);
+var file_names = [];
+var allLinks = [];
+var sectionLinks = [];
+var sections = [].slice.apply(document.getElementsByClassName('resource-section'));
+sections.forEach(function(element) {
+  var links = [];
+  var sectionFileNames = [];
+  var sectionTitle = element.querySelector('h2').innerText;
+  var anchorElements = [].slice.apply(element.getElementsByTagName('a'));
+  anchorElements.forEach(function(anchor) {
+    if (window.location.href.indexOf("resource") > -1) {
+      var href = anchor.href;
+      var hashIndex = href.indexOf('#');
+      if (hashIndex >= 0) {
+        href = href.toLowerCase().substr(0, hashIndex);
+      }
+      var res = 'get_resource';
+      if (res === href.substr(33, res.length)) {
+        links.push(href);
+        sectionFileNames.push(anchor.text);
+        file_names.push(anchor.text);
+      }
+    }
+  });
+
+  // Remove duplicates
+  var uniqueLinks = [];
+  var uniqueFileNames = [];
+  for (var i = 0; i < links.length; i++) {
+    if (uniqueLinks.indexOf(links[i]) === -1) {
+      uniqueLinks.push(links[i]);
+      uniqueFileNames.push(sectionFileNames[i]);
+    }
   }
-  var res = 'get_resource';
-  if (res == href.substr(33, res.length)) 
-  {
-   file_names.push(element.text);
-  }
-  return href;
-}
+
+  sectionLinks.push({
+    sectionTitle: sectionTitle,
+    links: uniqueLinks,
+    file_names: uniqueFileNames
+  });
 });
 
-// Remove duplicates and invalid URLs.
-var res = 'get_resource';
-for (var i = 0; i < links.length;) {
-  if (((i > 0) && (links[i] == links[i - 1])) ||
-      (links[i] == '') || (links[i] == null) || (links[i] == undefined) ||
-      (res != links[i].toLowerCase().substr(33, res.length))) 
-  {
-    links.splice(i, 1);
-  } 
-  else 
-  {
-      ++i;
-  }
-  
-}
+var courseName = document.querySelector('h1.mb-1').innerText;
 
-chrome.runtime.sendMessage({ type: "links", data: links });
-chrome.runtime.sendMessage({ type: "links", data: file_names });
+chrome.runtime.sendMessage({ type: "links", data: sectionLinks, courseName: courseName });
